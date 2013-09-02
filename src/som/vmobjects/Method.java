6eb91f6142ca2304ba8713a982912c1485d491bb
Stefan Marr <git@stefan-marr.de>
2013-09-02 22:50:30 +0200

    Changed spelling
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/vmobjects/Method.java
+++ src/som/vmobjects/Method.java
@@ -204,7 +204,7 @@ public class Method extends Array implements Invokable {
   private java.util.ArrayList<Invokable> invokedMethods                    = new java.util.ArrayList<Invokable>();
   private int                            receiverClassIndex                = 0;
 
-  // Private variable holding number of invocations and backedges
+  // Private variable holding number of invocations and back edges
   private long                           invocationCount;
 
   // Private variable holding byte array of bytecodes
