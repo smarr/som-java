36a2a13735042a785c159b10989eaa6873e4390b
Stefan Marr <git@stefan-marr.de>
2019-01-17 14:14:06 +0000

    Make som.sh robust to spaces and jar not being built
    - use quotes to delimit path elements
    - add build/classes to avoid having to run `ant jar`
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- som.sh
+++ som.sh
@@ -3,4 +3,4 @@ pushd `dirname $0` > /dev/null
 SCRIPT_PATH=`pwd`
 popd > /dev/null
 
-java -server -cp ${SCRIPT_PATH}/build/som.jar som.vm.Universe "$@"
+java -server -cp "${SCRIPT_PATH}/build/classes":"${SCRIPT_PATH}/build/som.jar" som.vm.Universe "$@"
